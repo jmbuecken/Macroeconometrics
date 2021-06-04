5fe6a2fa18c006fde856d24223e7381feb2708aa
jmbuecken <71153413+jmbuecken@users.noreply.github.com>
2021-06-04 11:34:51 +0200

    Add files via upload

--- code.R
+++ code.R
@@ -1,1 +1,18 @@
 ### Macroeconomics Project ###
+
+rm(list = ls())
+
+# read data
+oil_prices <- read_csv("Downloads/oil-import-prices-all-countries.csv")
+
+# drop all countries, keep Austria
+temp_oil_prices_aut <- oil_prices[which(oil_prices$LOCATION=='AUT'),]
+
+# delete unnecessary columns
+temp <- c("TIME", "Value")
+oil_prices_aut <- temp_oil_prices_aut[temp]
+rm(temp_oil_prices_aut)
+
+# plot 1st order difference
+plot(oil_prices_aut[1:40], oil_prices_aut[2:41], 'l')
+plot(oil_prices_aut$Value[1:40], oil_prices_aut$Value[2:41],)
